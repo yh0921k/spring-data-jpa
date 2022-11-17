be67cb6ecbe20b8a531b288131c40fb8602ced61
Yonghwi Kim <yh0921k@gmail.com>
2022-11-18 08:44:26 +0900

    init: Spring Data JPA 학습을 위한 저장소 초기화
    README.md
    - 저장소의 기반이 되는 강의 내용 추가
    
    .gitignore
    - Git 추적 제외 파일 추가
    
    buile.gradle
    - H2 Database 의존성 버전 변경

---
+++ README.md
@@ -0,0 +1,3 @@
+### Spring Data JPA
+*** 
+해당 저장소는 김영한님의 Spring Data JPA 강의를 수강하며 진행한 예제입니다.([강의 링크](https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-%EB%8D%B0%EC%9D%B4%ED%84%B0-JPA-%EC%8B%A4%EC%A0%84))
---
+++ src/test/java/study/jpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,11 @@
+package study.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
