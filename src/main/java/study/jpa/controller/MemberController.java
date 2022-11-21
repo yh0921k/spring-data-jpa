9df30f488e44e76001ffecb157689c1e281ca62d
Yonghwi Kim <yh0921k@gmail.com>
2022-11-21 12:15:29 +0900

    add: Section 06. 새로운 엔티티를 구별하는 방법

--- src/main/java/study/jpa/controller/MemberController.java
+++ src/main/java/study/jpa/controller/MemberController.java
@@ -36,10 +36,10 @@ public class MemberController {
     return member.getUsername();
   }
 
-  @PostConstruct
-  public void init() {
-    for (int i = 0; i < 100; i++) {
-      memberRepository.save(new Member("Member" + i, i));
-    }
-  }
+//  @PostConstruct
+//  public void init() {
+//    for (int i = 0; i < 100; i++) {
+//      memberRepository.save(new Member("Member" + i, i));
+//    }
+//  }
 }
---
+++ src/test/java/study/jpa/repository/ItemRepositoryTest.java
@@ -0,0 +1,18 @@
+package study.jpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.jpa.entity.Item;
+
+@SpringBootTest
+class ItemRepositoryTest {
+
+  @Autowired ItemRepository itemRepository;
+
+  @Test
+  public void save() {
+    Item item = new Item("A");
+    itemRepository.save(item);
+  }
+}
